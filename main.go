0425e21dd073c1fd943278665550bb2a67415036
Philip Griesbacher <philip@griesbacher.org>
2017-03-22 08:57:52 +0100

    added example callback

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 
+	"unsafe"
+
 	"github.com/ConSol/go-neb-wrapper/neb"
+	"github.com/ConSol/go-neb-wrapper/neb/naemon"
 )
 
 // Build contains the current git commit id
@@ -11,10 +14,17 @@ import (
 var Build string
 
 //This is an example main file, which should demonstrate how to use the library.
-func main() {
+func init() {
 	neb.Title = "GO GO Neb Wrapper!"
 	neb.Desc = "This is an example how to use the go neb wrapper"
 	neb.License = "GPL v3"
 	neb.Version = fmt.Sprintf("1.0.0 - %s", Build)
 	neb.Author = "Philip Griesbacher / Sven Nierlein"
+	exampleCallback := func(int, unsafe.Pointer) int {
+		fmt.Println("Example Callback")
+		return neb.NebOk
+	}
+
+	neb.AddCallback(naemon.NebcallbackProcessData, exampleCallback)
 }
+func main() {}
