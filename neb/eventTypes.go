dd0aeb9e6cc7c06fd1aaadac343f4812528e7380
Philip Griesbacher <philip@griesbacher.org>
2017-03-27 11:40:06 +0200

    added Serviceeventtypes

--- neb/eventTypes.go
+++ neb/eventTypes.go
@@ -20,6 +20,11 @@ extern struct service *service_list;
 import "C"
 
 const (
+	//ServicecheckInitiate a check of the route to the host has been initiated
+	ServicecheckInitiate = C.NEBTYPE_SERVICECHECK_INITIATE
+	//ServicecheckProcessed the processed/final result of a service check
+	ServicecheckProcessed = C.NEBTYPE_SERVICECHECK_PROCESSED
+
 	//HostcheckInitiate a check of the route to the host has been initiated
 	HostcheckInitiate = C.NEBTYPE_HOSTCHECK_INITIATE
 	//HostcheckProcessed the processed/final result of a host check
