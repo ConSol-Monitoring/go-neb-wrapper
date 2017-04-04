3642bfe86c6e893cb6bd7fd4f951d2e91843f0d3
Philip Griesbacher <philip@griesbacher.org>
2017-04-04 15:30:33 +0200

    nagios3 compatible

--- neb/checkTypes/checkTypes.go
+++ neb/checkTypes/checkTypes.go
@@ -2,10 +2,10 @@ package checkTypes
 
 /*
 
-#cgo nagios3 CFLAGS: -DNAGIOS3 -I. -I${SRCDIR}/../libs
+#cgo nagios3 CFLAGS: -DNAGIOS3 -I. -I${SRCDIR}/../../libs
 #cgo nagios3 LDFLAGS: -Wl,-unresolved-symbols=ignore-all
 
-#cgo nagios4 CFLAGS: -DNAGIOS4 -I. -I${SRCDIR}/../libs
+#cgo nagios4 CFLAGS: -DNAGIOS4 -I. -I${SRCDIR}/../../libs
 #cgo nagios4 LDFLAGS: -Wl,-unresolved-symbols=ignore-all
 
 #cgo naemon CFLAGS: -DNAEMON -I.
@@ -15,26 +15,46 @@ package checkTypes
 
 */
 import "C"
+import "github.com/ConSol/go-neb-wrapper/neb"
 
-const (
+var (
 	//Active for passive checks
-	Active = C.CHECK_TYPE_ACTIVE
+	Active int
 	//Passive for active checks
-	Passive = C.CHECK_TYPE_PASSIVE
+	Passive int
 	//Parent (active) check for the benefit of dependent object
-	Parent = C.CHECK_TYPE_PARENT
+	Parent int
 	//File from spool files (yuck)
-	File = C.CHECK_TYPE_FILE
+	File int
 	//Other for modules to use
-	Other = C.CHECK_TYPE_OTHER
+	Other int
 )
 
-var checkTypeMapping = map[int]string{
-	Active:  "Active",
-	Passive: "Passive",
-	Parent:  "Parent",
-	File:    "File",
-	Other:   "Other",
+var checkTypeMapping map[int]string
+
+func init() {
+	switch neb.GetCoreType() {
+	case neb.CoreNagios3:
+		Active = C.SERVICE_CHECK_ACTIVE
+		Passive = C.SERVICE_CHECK_PASSIVE
+		Parent = -1
+		File = -1
+		Other = -1
+	case neb.CoreNaemon, neb.CoreNagios4:
+		Active = C.CHECK_TYPE_ACTIVE
+		Passive = C.CHECK_TYPE_PASSIVE
+		Parent = C.CHECK_TYPE_PARENT
+		File = C.CHECK_TYPE_FILE
+		Other = C.CHECK_TYPE_OTHER
+	}
+
+	checkTypeMapping = map[int]string{
+		Active:  "Active",
+		Passive: "Passive",
+		Parent:  "Parent",
+		File:    "File",
+		Other:   "Other",
+	}
 }
 
 func CheckTypeToString(checkType int) string {
